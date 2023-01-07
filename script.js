c345a6df7a6595806c7557ea04ccb6b0fe712b3b
Geraldine Acevedo <geraldineacevedo29@gmail.com>
2023-01-07 08:50:17 -0300

    agrego consignas del proyecto y menciono las herramientas que utilice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+//El lenguaje de programacion JavaScript
+//permite darle "comportamiento" a las paginas web
+//en el navegador al hacer click en un boton,
+//al enviar un formulario, al desplegar un menu, etc.
+//Los usuarios podran interactuar con nuestros sitios.
+
+alert("Este proyecto de CV es para la entrega final del curso 'Primeros pasos del desarrollo frontend' impartido por 'Ticmas Academy' para 'Argentina Programa 4.0'. Utilice un personaje ficticio de https://randomuser.me para crear el CV, en este proyecto me guie con Cards de Bootstrap para cada seccion, le agregue iconos de Font Awesome y fui plasmando lo aprendido en el curso sobre Git, HTML, CSS, el framework Bootstrap para hacerlo Responsive, algo de JavaScript, y lo subi a un repositorio en Github. ");
 
