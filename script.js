c38d629a6646ced4de507bdd38ad2a95cf9ca2c3
Geraldine Acevedo <geraldineacevedo29@gmail.com>
2023-03-24 16:15:51 -0300

    Agrego fetch de javascript

--- script.js
+++ script.js
@@ -6,6 +6,30 @@
 
 alert("Este proyecto de CV es para la entrega final del curso 'Primeros pasos del desarrollo frontend' impartido por 'Ticmas Academy' para 'Argentina Programa 4.0'. Utilice un personaje ficticio de https://randomuser.me para crear el CV, en este proyecto me guie con Cards de Bootstrap para cada seccion, le agregue iconos de Font Awesome y fui plasmando lo aprendido en el curso sobre Git, HTML, CSS, el framework Bootstrap para hacerlo Responsive, algo de JavaScript, y lo subi a un repositorio en Github. ");
 
-function infoFoto(){
-    alert("Hello everyone! My name is Carrie Jackson. I'\m from USA");
-}
+
+
+fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("image").src =
+            data.results[0].picture.large;
+            document.getElementById("name").innerHTML =
+            data.results[0].name.first +" "+ data.results[0].name.last;
+            
+            document.getElementById("text").innerHTML =
+            "Hello everyone! My name is "+ data.results[0].name.first 
+            +", I'\m from "+ data.results[0].location.country;
+            document.getElementById("phone").innerHTML =
+            data.results[0].phone;
+            document.getElementById("email").innerHTML =
+            data.results[0].email;
+            document.getElementById("location").innerHTML =
+            data.results[0].location.state +", "+ data.results[0].location.country; 
+        });
+
+
+        function infoFoto(){
+            
+            alert(
+            `Open to work!`);
+        }
